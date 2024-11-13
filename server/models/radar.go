cb81034c8ca7ef8505a9fad2bae56e9c36f69397
yolimpiev <yr.olimpiev@gmail.com>
2024-11-13 11:57:31 +0700

    Remove extra

--- server/models/radar.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Radar struct {
-    ID        string  `json:"id"`
-    Latitude  float64 `json:"latitude"`
-    Longitude float64 `json:"longitude"`
-    Coverage  float64 `json:"coverage"` // сектор покрытия в градусах
-}
