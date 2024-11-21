715a930c2b41825b3206d78da42887a8ceb1d316
yolimpiev <yr.olimpiev@gmail.com>
2024-11-21 10:05:22 +0700

    Fix websocket

--- server/usecases/drone_usecase.go
+++ server/usecases/drone_usecase.go
@@ -1,32 +1,114 @@
-package usecase
+package usecases
 
 import (
+	"drone_server/events"
 	"drone_server/models"
 	repository "drone_server/repositories"
+	"math"
+	"sync"
+	"time"
 )
 
 type DroneUsecase interface {
-	GetAllDrones() []*models.Drone
-	AddNewDrone(drone *models.Drone)
-	UpdateDroneInfo(id string, drone *models.Drone)
+	GetDrones() ([]models.Drone, error)
+	AddDrone(drone models.Drone) error
+	UpdateDrone(drone models.Drone) error
+	DeleteDrone(id int) error
 }
 
-type droneUsecase struct {
-	repo repository.DroneRepository
+type droneUsecaseImpl struct {
+	repo         repository.DroneRepository
+	eventEmitter *events.EventEmitter
+	mu           sync.Mutex
+	stopChan     chan struct{}
 }
 
-func NewDroneUsecase(repo repository.DroneRepository) DroneUsecase {
-	return &droneUsecase{repo: repo}
+func NewDroneUsecase(repo repository.DroneRepository, emitter *events.EventEmitter) DroneUsecase {
+	uc := &droneUsecaseImpl{
+		repo:         repo,
+		eventEmitter: emitter,
+		stopChan:     make(chan struct{}),
+	}
+
+	go uc.startDroneMovement()
+	return uc
 }
 
-func (uc *droneUsecase) GetAllDrones() []*models.Drone {
-	return uc.repo.GetDrones()
+func (uc *droneUsecaseImpl) AddDrone(drone models.Drone) error {
+	uc.mu.Lock()
+	defer uc.mu.Unlock()
+	uc.repo.Add(drone)
+	return nil
 }
 
-func (uc *droneUsecase) AddNewDrone(drone *models.Drone) {
-	uc.repo.AddDrone(drone)
+func (u *droneUsecaseImpl) UpdateDrone(drone models.Drone) error {
+	u.mu.Lock()
+	defer u.mu.Unlock()
+	if err := u.repo.Update(drone); err != nil {
+		return err
+	}
+	// Генерация события при обновлении дрона
+	u.eventEmitter.Emit("drone_updated", drone)
+	return nil
 }
 
-func (uc *droneUsecase) UpdateDroneInfo(id string, drone *models.Drone) {
-	uc.repo.UpdateDrone(id, drone)
+func (u *droneUsecaseImpl) GetDrones() ([]models.Drone, error) {
+	u.mu.Lock()
+	defer u.mu.Unlock()
+	return u.repo.GetAll()
+}
+
+func (uc *droneUsecaseImpl) DeleteDrone(id int) error {
+	uc.mu.Lock()
+	defer uc.mu.Unlock()
+	uc.repo.Delete(id)
+	return nil
+}
+
+// Симуляция движения дронов
+func (uc *droneUsecaseImpl) startDroneMovement() {
+	ticker := time.NewTicker(3 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			uc.moveDrones()
+		case <-uc.stopChan:
+			return
+		}
+	}
+}
+
+// Остановка симуляции движения
+func (uc *droneUsecaseImpl) Stop() {
+	close(uc.stopChan)
+}
+
+func (uc *droneUsecaseImpl) moveDrones() {
+	uc.mu.Lock()
+	defer uc.mu.Unlock()
+
+	const centerLat = 54.843243
+	const centerLon = 83.088801
+	const degreePerMeter = 1.0 / 111000.0 // Приблизительно 1 градус ≈ 111 км
+
+	drones, _ := uc.repo.GetAll()
+	for i, drone := range drones {
+		// Угол для текущего дрона
+		angle := float64(time.Now().UnixNano()/1e9) + float64(i)*2*math.Pi/float64(len(drones))
+		radius := 300.0 + float64(i)*50.0 // Радиус в метрах
+
+		// Смещение по широте и долготе
+		deltaLat := radius * math.Cos(angle) * degreePerMeter
+		deltaLon := radius * math.Sin(angle) * degreePerMeter
+
+		// Обновляем координаты
+		drone.Latitude = centerLat + deltaLat
+		drone.Longitude = centerLon + deltaLon
+
+		// Сохраняем в репозиторий и отправляем обновление
+		uc.repo.Update(drone)
+		uc.eventEmitter.Emit("drone_updated", drone)
+	}
 }
